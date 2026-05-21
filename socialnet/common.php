03f3adf763237e37947d80780e1f4c2b1b5f47e2
Mike James <mikejames1311005@gmail.com>
2026-05-21 21:44:59 +0700

    Set HttpOnly, Secure and SameSite flags on session cookie

--- socialnet/common.php
+++ socialnet/common.php
@@ -1,4 +1,12 @@
 <?php
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path'     => '/',
+    'domain'   => '',
+    'secure'   => true,
+    'httponly' => true,
+    'samesite' => 'Lax',
+]);
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
